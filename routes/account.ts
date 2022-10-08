063731cd745e3f41023a48207222c9b65a7c94d0
lirenyeServer <2694613297@qq.com>
2022-10-08 23:08:30 +0800

    完成账户信息查询接口

--- routes/account.ts
+++ routes/account.ts
@@ -4,6 +4,7 @@ import logger from "../utils/logger";
 
 const AccountRouter = Router();
 
+// 添加账户信息接口
 AccountRouter.post('/add',async (req: Request, res: Response)=>{
   // 检验数据
   try {
@@ -22,6 +23,24 @@ AccountRouter.post('/add',async (req: Request, res: Response)=>{
   return res.send({data: null, meta: {status: 200, mes: '保存成功'}});
 });
 
+
+// 查询账户信息接口
+AccountRouter.get('/info', async (req: Request, res: Response)=>{
+  // 检验参数
+  const allow = ['platform', 'mobile', 'email'];
+  const queryType = Object.keys(req.body)
+  if(allow.indexOf(queryType[0]) === -1) return res.send({data: null, meta: {status: 201, msg: '查询参数错误'}});
+
+  // 查询数据
+  try{
+    var dbData = await AccountModel.find(req.body)
+  }catch(error){
+    return res.send({data: null, meta: { status: 201, msg: '查询错误'}})
+  };
+
+  // 返回查询数据
+  return res.send({data: dbData, total: dbData.length, meta: {status: 200, msg: '查询成功'}})
+});
 
 
 export default AccountRouter;
