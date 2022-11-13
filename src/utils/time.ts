40ba016eda093a40d8ca44a2bc244634828f5813
lirenyeServer <2694613297@qq.com>
2022-11-13 12:18:13 +0800

    增加输出日志函数

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,5 +3,10 @@ function LocalTime():number {
   return (date.getTime() + 28800000);
 };
 
+export function FormatLog(type: ('ERROR' | 'INFO'), username: string, msg: string):void{
+  const localTime = LocalTime();
+  const nowTime = new Date(localTime);
+  console.log(`[${nowTime.toLocaleString()}] ${type} (${username}): ${msg}`);
+}
 
 export default LocalTime;
